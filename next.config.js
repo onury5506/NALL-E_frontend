02cb7d6b1a42fde8f32180e7110bb4763a7a8a63
onury5506 <onuryildizsai@gmail.com>
2023-02-16 14:11:05 +0300

    feature: dependencies and env variables

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['nalle.s3.eu-west-3.amazonaws.com'],
+  },
+  env: {
+    GRAPHQL_SERVER: process.env.GRAPHQL_SERVER,
+    CONTRACT_ADDRESS: process.env.CONTRACT_ADDRESS
+  }
 }
 
 module.exports = nextConfig
