371c14faf7d35e08db533e9c7b743a4d6ed609c2
onury5506 <onuryildizsai@gmail.com>
2023-02-16 14:16:04 +0300

    feature: Navbar Component

---
+++ components/navbar/navbar.jsx
@@ -0,0 +1,23 @@
+import Link from 'next/link'
+
+import Button from '../button/button'
+import Container from '../container/container'
+
+import styles from './navbar.module.css'
+
+export default function Navbar() {
+
+    return (
+        <nav id={styles.navbar}>
+            <Container id={styles["navbar-content"]} size="md">
+                <div id={styles.logo}>
+                    <Link href="/">
+                        <h1>NALL-E</h1>
+                    </Link>
+                </div>
+                <Button>Get In Touch</Button>
+            </Container>
+        </nav>
+    )
+
+}
