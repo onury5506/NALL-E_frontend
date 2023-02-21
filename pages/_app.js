a759b2d910432b75519b84f1bdf33e1ef1734db3
onury5506 <onuryildizsai@gmail.com>
2023-02-21 10:40:47 +0300

    feature: google analytics integrated

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
 import '@/styles/globals.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
+import { GoogleAnalytics } from 'nextjs-google-analytics'
+
 config.autoAddCss = false
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <GoogleAnalytics trackPageViews />
+    <Component {...pageProps} />
+  </>
 }
